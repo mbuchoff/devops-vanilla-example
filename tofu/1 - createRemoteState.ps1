3b2ea0ff46362d070eb21476654a9b8e44ef71e6
Michael Buchoff <mbuchoff@gmail.com>
2024-05-22 19:29:46 -0400

    Quoted steps

--- tofu/1 - createRemoteState.ps1
+++ tofu/1 - createRemoteState.ps1
@@ -3,5 +3,5 @@ $last4Sub = $subscriptionId.Substring($subscriptionId.Length -4)
 $rgName = "rg-devopsvanillaexample-" + $last4Sub
 $saName = "sadevopsvanillaexamp" + $last4Sub
 az group create --name $rgName -l eastus
-az storage account create --name $saName --resource-group $rgName --location eastus --sku Standard_LRS
-az storage container create -n terraform --account-name $saName
+az storage account create --name "$saName" --resource-group "$rgName" --location eastus --sku Standard_LRS
+az storage container create -n terraform --account-name "$saName"
